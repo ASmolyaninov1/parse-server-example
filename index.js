f01f06d68a0641fbfee294ed3fb67e363b3098df
ASmolyaninov <a.smolyaninov@brightlab.me>
2021-12-24 14:51:39 +0300

    init commit

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ if (!databaseUri) {
 const config = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'c9f6596b7bc52a225bde57b69fcb14c4',
+  masterKey: process.env.MASTER_KEY || '1fd0f508cda8ff602f86f6d709593311', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://dockerhost.forge-parse-server.c66.me:40126/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
